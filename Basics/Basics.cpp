6fd0bb9a6e0c92a4b87712c45f2bd1a57de79275
Anthony Tleiji <anthonytleiji@gmail.com>
2022-04-21 14:51:19 +0300

    Added add() function to Chapter 1

--- Basics/Basics.cpp
+++ Basics/Basics.cpp
@@ -9,6 +9,21 @@ void multiply()
 	std::cout << "Triple that number is: " << numberToMultiply * 3 << '\n';
 }
 
+void add()
+{
+	int number1{};
+	int number2{};
+
+	std::cout << "Enter an integer: ";
+	std::cin >> number1;
+
+	std::cout << "Enter another integer: ";
+	std::cin >> number2;
+
+	std::cout << number1 << "+" << number2 << "=" << number1 + number2 << '\n';
+	std::cout << number1 << "-" << number2 << "=" << number1 - number2 << '\n';
+}
+
 int main()
 {
 	//std::cout << "Enter a number: ";
@@ -17,6 +32,7 @@ int main()
 	//std::cout << "You entered " << x << '\n';
 
 	multiply();
+	add();
 
 	return 0;
 }
